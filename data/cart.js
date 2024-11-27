7f069e34c26b51b5ba7d9f5e97bd6b3fe38a0851
Amazon <2361660078@qq.com>
2024-11-27 17:06:14 +0800

    保存数据，并为送达时间生成HTML代码

--- data/cart.js
+++ data/cart.js
@@ -2,11 +2,13 @@ export let cart = JSON.parse(localStorage.getItem('cart')) ||
 [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }
 ];
 
@@ -29,7 +31,8 @@ export function addToCart(productId){
   else {//如果购物车中没有当前的物品，则在数组中加入产品名称即数量
     cart.push({
     productId: productId,
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '1'
   });
 }
   saveToStorage();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,20 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";//一个点表示当前文件夹
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+//不使用花括号,默认导出
+import { deliveryOptions } from "../data/deliveryOptions.js";
+
+
+hello();
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+
+console.log(deliveryDate.format('dddd, MMMM D'));
+console.log(deliveryDate);
+
 
 let cartSummeryHTML = '';
 
@@ -17,12 +31,28 @@ cart.forEach((cartItem)=>{
       matchingProduct = product
     }
   });
+  const deliveryOptionId = cartItem.deliveryOptionId;
+  let deliveryOption;
 
+  deliveryOptions.forEach((option)=>{
+    if(option.id === deliveryOptionId){
+      deliveryOption = option;
+    };
+  });
 
-    cartSummeryHTML+=` <div class="cart-item-container 
+  const today = dayjs();
+  const deliveryDay = today.add(
+    deliveryOption.deliveryDays,
+    'days'
+  )
+  const dateString = deliveryDay.format(
+    'dddd, MMMM D'
+  );
+
+  cartSummeryHTML+=` <div class="cart-item-container 
     js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -58,49 +88,53 @@ cart.forEach((cartItem)=>{
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHTML(matchingProduct,cartItem)}
+          
         </div>
       </div>
     </div>`;
 });
+
+function deliveryOptionsHTML(matchingProduct,cartItem){
+  let html = '';
+
+  deliveryOptions.forEach((deliveryOption)=>{
+    const today = dayjs();
+    const deliveryDay = today.add(
+      deliveryOption.deliveryDays,
+      'days'
+    )
+    const dateString = deliveryDay.format(
+      'dddd, MMMM D'
+    );
+    const priceString = deliveryOption.priceCents === 0 
+    ? 'FREE'
+    : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+    html +=
+    `
+      <div class="delivery-option">
+        <input type="radio" 
+        ${isChecked ? 'checked' : ''}
+          class="delivery-option-input"
+          name="delivery-option-${matchingProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${dateString}
+          </div>
+          <div class="delivery-option-price">
+            ${priceString} Shipping
+          </div>
+        </div>
+      </div>
+    `
+  });
+  return html;
+}
+
+
+
 document.querySelector('.js-order-summery').innerHTML = cartSummeryHTML;
 
 // 实现从结算页面删除产品需要
