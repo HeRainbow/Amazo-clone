7489a06db3d0318a24ad0ffb721438ed2c25b2d2
Amazon <2361660078@qq.com>
2024-12-01 23:44:57 +0800

    标准化命名测试文件

---
+++ tests-jasmine/tests-simple/moneyTest.js
@@ -0,0 +1,23 @@
+import {formatCurrency} from "../../scripts/utils/money.js"
+
+console.log('')
+
+console.log('converts cents to dollars');
+if(formatCurrency(2095) === '20.95'){
+  console.log('passed');
+}else{
+  console.log('failed');
+}
+
+if(formatCurrency(0) === '0.00'){
+  console.log('passed');
+}else{
+  console.log('failed');
+}
+
+
+if(formatCurrency(2000.4) === '20.00'){
+  console.log('passed');
+}else{
+  console.log('failed')
+}
