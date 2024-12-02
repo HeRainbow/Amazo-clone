7e479c1c64691543b656caf5e8397f131d844b7a
Amazon <2361660078@qq.com>
2024-12-02 20:42:45 +0800

    将XHR http改为Fetch

--- tests/checkout/orderSummeryTest.js
+++ tests/checkout/orderSummeryTest.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummery.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts,loadProductsFetch } from "../../data/products.js";
 
 
 
@@ -9,8 +9,8 @@ describe('test suite: renderOrderSummery',() => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeAll((done)=>{
-    loadProducts(()=>{
-      done();//只有等done()执行之后才继续执行后续代码
+    loadProductsFetch().then(()=>{
+      done();
     });
   });
   beforeEach(() => {//先执行这段代码再执行其他it
