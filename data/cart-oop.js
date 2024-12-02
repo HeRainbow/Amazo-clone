f3c7edacfce17eb5d6ee57754a5a20211d9544cc
Amazon <2361660078@qq.com>
2024-12-02 11:47:10 +0800

    为cart创建面向对象编程版本

---
+++ data/cart-oop.js
@@ -0,0 +1,118 @@
+
+function Cart(localStorageKey){
+  const cart = {
+    cartItems: undefined,
+  
+    loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) ||
+    [
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+      },
+      {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+      }
+    ];
+    },
+  
+    saveToStorage () {
+      localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems));
+    },
+  
+  
+  
+    addToCart(productId){
+      let matchingItem;//记录是否当前物品已经在购物车里存在
+    
+      this.cartItems.forEach((cartItem)=>{
+        if(productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+    
+      if(matchingItem){//如果购物车中已经有当前的物品，则计数加一
+        matchingItem.quantity +=1;
+      }
+      else {//如果购物车中没有当前的物品，则在数组中加入产品名称即数量
+        this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: '1'
+      });
+    }
+      this.saveToStorage();
+    },
+  
+    
+  
+  
+  // 1.创建一个新的数组
+  // 2.遍历旧数组
+  // 3.将除了productId之外的所有值都存入新数组
+    removeFromCart(productId){
+      this.cartItems =  this.cartItems.filter((cartItem)=>{
+        if(cartItem.productId === productId){
+          return false
+        }else{
+          return true
+        }
+      });
+      
+      this.saveToStorage();
+    },
+  
+    updateDeliveryOption(productId, deliveryOptionId){
+      let matchingItem;//记录是否当前物品已经在购物车里存在
+    
+      this.cartItems.forEach((cartItem)=>{
+        if(productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+      matchingItem.deliveryOptionId = deliveryOptionId;
+      this.saveToStorage();
+    },
+  
+    calculateCartQuantity() {
+      let cartQuantity = 0;//计算当前购物车中物品总数
+      this.cartItems.forEach((cartItem)=>{
+        cartQuantity += cartItem.quantity;
+      });
+      return cartQuantity;
+    },
+  
+    updateQuantity(productId, newQuantity){
+      this.cartItems.forEach((cartItem)=>{
+        if(cartItem.productId === productId){
+          cartItem.quantity = newQuantity;
+        };
+      });
+    
+      this.saveToStorage();
+    }
+    
+  };
+
+  return cart;
+}
+
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
+
+
+
+
+
+
+cart.loadFromStorage();
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
+
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,9 @@
 import { renderOrderSummary } from "./checkout/orderSummery.js";
 import { renderPamentSummery } from "./checkout/paymentSummery.js";
+import '../data/cart-oop.js';
+
+
+
 renderOrderSummary();
 //使用DOM来改变页面HTML需要刷新才能看见改变，生成HTML则即时修改
 //此技术称为MVC  
