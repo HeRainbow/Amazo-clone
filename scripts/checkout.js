f00480de68f5e497db6f697258859c879c8f2590
Amazon <2361660078@qq.com>
2024-12-02 20:23:43 +0800

    学习promise并将代码转换为promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,8 +4,26 @@ import { renderPamentSummery } from "./checkout/paymentSummery.js";
 // import '../data/backend-pratice.js'
 import { loadProducts } from "../data/products.js";
 
+Promise.all([
+
+]).then(()=>{
+  
+});
 
 
+//Promise是一个内置类，在创建时会立即执行
+new Promise((resolve)=>{//resolve是一个函数，与done（）类似
+  loadProducts(()=>{
+    resolve();
+  });
+}).then(()=>{//创建了一个新的线程
+
+
+  renderOrderSummary();
+  renderPamentSummery();
+})
+
+/*
 loadProducts(()=>{
   renderOrderSummary();
   //使用DOM来改变页面HTML需要刷新才能看见改变，生成HTML则即时修改
@@ -16,3 +34,4 @@ loadProducts(()=>{
   
   renderPamentSummery();
 });
+*/
